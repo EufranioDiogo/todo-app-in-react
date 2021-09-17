0f5498e4cb2a42adcc68ecfebef48a87d6414d43
EufranioDiogo <eufraniodiogo5@gmail.com>
2021-09-17 09:16:06 +0100

    Adding random Id and with empty task list first

--- FirstReactApp/react-task-tracker/src/components/MainApp.js
+++ FirstReactApp/react-task-tracker/src/components/MainApp.js
@@ -7,24 +7,6 @@ import AddTask from './AddTask';
 const MainApp = function() {
     const [showForm, setShowForm] = useState(false);
     const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            text: 'Something',
-            day: 'Out 9 at 20:00pm',
-            reminder: false
-        },
-        {
-            id: 2,
-            text: 'Something2',
-            day: 'Out 10 at 22:00pm',
-            reminder: false
-        },
-        {
-            id: 3,
-            text: 'Something3',
-            day: 'Out 12 at 80:00pm',
-            reminder: true
-        }
     ]);
 
     const deleteTask = function (taskId) {
@@ -43,7 +25,7 @@ const MainApp = function() {
     }
 
     const addTask = function (task) {
-        task.id = tasks.length + 1;
+        task.id = Math.floor(Math.random() * 1001);
         setTasks(tasks.concat([task]));
     }
 
